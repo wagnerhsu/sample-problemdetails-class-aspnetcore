b7e2ae30388b24963170f2b7d2b99b31773a041a
Wagner Xu <wagnerhsu@hotmail.com>
2021-12-07 15:44:10 +0800

    Add Initial migrations

---
+++ ErrorHandlingProblemDetails/Data/AppDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ErrorHandlingProblemDetails.Data;
+
+public class AppDbContextFactory: IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var configuration = BuildConfiguration();
+
+        var builder = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+        return new AppDbContext(builder.Options);
+    }
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
--- ErrorHandlingProblemDetails/README.md
+++ ErrorHandlingProblemDetails/README.md
@@ -1,6 +1,7 @@
 ﻿## 2021-12-07
 - 由于无法处理前端"yyyy-MM-dd HH:mm:ss"这样的时间格式，所以可以改用Newtonsoft.Json
 - 也可以使用`AddJsonOptions`并定义`JsonConverter`来解决上述时间格式问题
+- Add `IDesignTimeDbContextFactory` support
 ## 2021-12-03
 - Can persist to sqlserver
 
